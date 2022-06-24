3ba79bba58ec9c60e31ce862dc1c7dbd0e05266d
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2022-06-24 15:05:47 +0100

    connect to mongodb

--- my-blog-rest-api/index.js
+++ my-blog-rest-api/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(console.log('Connected to MongoDB'))
+.catch((err) => console.log(err))
 
 app.use('/', (req, res) => {
     console.log("hey this is main url");
